9faa680ff1c0351337cb305cdde075090c4edb26
Jason Yuan <yuanjason98@gmail.com>
2021-02-19 08:52:45 -0500

    moved processing/formatting scripts to preprocess_data

--- preproccess data.R
+++ preproccess data.R
@@ -1,10 +1,13 @@
 library(tidyverse)
+library(lubridate)
+library(ggpubr)
+library(rvest)
 rm(list = ls())
 
 raw_data = NULL
 for( i in 1:17){
   tmp = readxl::read_xlsx( "data/UofT Data Set.xlsx", skip = 1, sheet = i, 
-                                     col_names =  paste( "X", 1:12, sep="" ) ) %>% 
+                           col_names =  paste( "X", 1:12, sep="" ) ) %>% 
     mutate( Instructor_ID = i,
             PPL = X1,
             X1 = replace( X1, !str_detect(X1, "Student"), NA ),
@@ -15,8 +18,8 @@ for( i in 1:17){
 rm(tmp,i)
 
 names( raw_data  ) = c( "Student", "Year", "Month", "Day", "Aircraft", "LF_dual", 
-"LF_solo", "Instrument_AC",  "Instrument_Sim", "CC_dual", "CC_solo", "Exercises",
-"Instructor_ID", "Licence")
+                        "LF_solo", "Instrument_AC",  "Instrument_Sim", "CC_dual", "CC_solo", "Exercises",
+                        "Instructor_ID", "Licence")
 
 head(raw_data)
 
@@ -40,6 +43,12 @@ raw_data %>%
   select( Instructor_ID, Student_ID, Session_ID, Year, Month, Day, 
           Aircraft, Duration, Training_Type, Exercises, Licence ) -> clean_data
 
+getSeason <- function(month) {
+  ifelse(month >= 3 & month <= 5, "Spring",
+         ifelse(month >= 6 & month <= 8, "Summer",
+                ifelse(month >= 9 & month <= 11, "Fall", "Winter")))
+}
+
 clean_data_processed = clean_data %>% 
   distinct( Session_ID, .keep_all = T) %>% 
   # split the exercises string into a "list" column w str_split()
@@ -49,8 +58,20 @@ clean_data_processed = clean_data %>%
   # remove invalid exercises
   mutate(Exercises = as.integer(Exercises)) %>%
   filter(Exercises >= 1 & Exercises <= 30) %>%
-  distinct_all() 
+  distinct_all() %>%
+  mutate(Season = getSeason(Month)) %>%
+  mutate(Date = make_date(Year, Month, Day)) %>%
+  mutate(During_Covid = Year >= 2020)
 
-#View(clean_data_processed)
+# reading in the fuel per gallon price
+webpage <- read_html("https://www.indexmundi.com/commodities/?commodity=jet-fuel&months=240&currency=cad")
+tbls <- html_nodes(webpage, "table") %>%
+  html_table(fill = TRUE)
+fuel_prices <- as.data.frame(tbls[2])
+colnames(fuel_prices) <- c("Month_Year", "Price","Change")
+clean_data_processed$Month_Words <- month.abb[clean_data_processed$Month]
+clean_data_processed$Month_Year <- (str_c(clean_data_processed$Month_Words, clean_data_processed$Year, sep = " "))
 
-  
+clean_data_processed <- left_join(clean_data_processed,fuel_prices,by="Month_Year",copy = TRUE)
+clean_data_processed <- select(clean_data_processed, -c("Month_Year","Change","Month_Words"))
+colnames(clean_data_processed)[colnames(clean_data_processed) == 'Price'] <- 'Fuel Price Per Gallon'
